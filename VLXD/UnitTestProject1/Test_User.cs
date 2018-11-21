30e66fa35b4767ce333d6a12b581a6ef246023a9
linhdangjs <linhdang.js9896@gmail.com>
2018-11-21 15:33:32 +0700

    setup test_user

---
+++ VLXD/UnitTestProject1/Test_User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BUS;
+using DAO;
+using DTO.Entity;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Test_User
+    {
+        TaiKhoanBUS userBUS;
+        TaiKhoan user;
+        
+    }
+}
