615c63aed42b206a804074bff98a9fb1d80346ab
linhdangjs <linhdang.js9896@gmail.com>
2018-11-21 15:40:58 +0700

    test-loginNV

--- VLXD/UnitTestProject1/Test_Employee.cs
+++ VLXD/UnitTestProject1/Test_Employee.cs
@@ -8,10 +8,20 @@ using DTO.Entity;
 namespace UnitTest
 {
     [TestClass]
-    public class Test_NhanVien
+    public class Test_Employee
     {
         NhanVienBUS nvBUS;
         NhanVien nv;
+        
+        [TestMethod]
+        public void Test_LoginByNhanVien()
+        {
+            uBUS = new TaiKhoanBUS();
 
+            int expected = 2;
+            int actual = uBUS.DangNhapBUS("Tam", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
