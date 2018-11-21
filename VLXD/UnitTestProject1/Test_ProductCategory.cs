2ce6d99039ec29ddcaf704ee91770a973d19d4a9
longlongg <1451010098Long@ou.edu.vn>
2018-11-21 16:35:45 +0700

    setup test product-category

---
+++ VLXD/UnitTestProject1/Test_ProductCategory.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BUS;
+using DAO;
+using DTO.Entity;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Test_ProductCategory
+    {
+        LoaiSanPhamBUS lspBUS;
+        LoaiSanPham lsp;
+
+    }
+}
