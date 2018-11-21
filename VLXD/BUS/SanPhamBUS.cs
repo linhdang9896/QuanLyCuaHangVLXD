ce90a3fab2f43824b3224e44904a4d6f01ca58cc
linhdangjs <linhdang.js9896@gmail.com>
2018-11-21 15:14:23 +0700

    method loadSPBUS

---
+++ VLXD/BUS/SanPhamBUS.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO.Entity;
+
+namespace BUS
+{
+    public class SanPhamBUS
+    {
+        SanPhamDAO spDAO = new SanPhamDAO();
+
+        public List<SanPham> LoadSPBUS()
+        {
+            return spDAO.LoadSPDAO();
+        }
+
+    }
+}
