8462c1b52874fa7ef8f82a3536675e074ee00356
linhdangjs <linhdang.js9896@gmail.com>
2018-11-21 15:28:55 +0700

    setup unittest_login

--- VLXD/UnitTestProject1/Test_Login.cs
+++ VLXD/UnitTestProject1/Test_Login.cs
@@ -12,48 +12,5 @@ namespace UnitTest
     {
         TaiKhoanBUS uBUS;
 
-        [TestMethod]
-        public void Test_LoginByNhanVien()
-        {
-            uBUS = new TaiKhoanBUS();
-
-            int expected = 2;
-            int actual = uBUS.DangNhapBUS("Tam", "123");
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Test_LoginByQuanLy()
-        {
-            uBUS = new TaiKhoanBUS();
-
-            int expected = 1;
-            int actual = uBUS.DangNhapBUS("Long", "123");
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Test_LoginWrongID()
-        {
-            uBUS = new TaiKhoanBUS();
-
-            int expected = -1;
-            int actual = uBUS.DangNhapBUS("LongLong", "123");
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Test_LoginWrongPass()
-        {
-            uBUS = new TaiKhoanBUS();
-
-            int expected = -2;
-            int actual = uBUS.DangNhapBUS("Long", "321");
-
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
