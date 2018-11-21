6f33d349a765fa7139347d670fd4a14f902aa9b6
linhdangjs <linhdang.js9896@gmail.com>
2018-11-21 15:35:47 +0700

    test-add-update-user

--- VLXD/UnitTestProject1/Test_User.cs
+++ VLXD/UnitTestProject1/Test_User.cs
@@ -12,6 +12,38 @@ namespace UnitTest
     {
         TaiKhoanBUS userBUS;
         TaiKhoan user;
-        
+        [TestMethod]
+        public void Test_AddUser_Suscess()
+        {
+            userBUS = new TaiKhoanBUS();
+            Assert.AreEqual(3, userBUS.LoadUserBUS().Count);
+
+            user = new TaiKhoan();
+            user.TenTaiKhoan = "Thuy321";
+            user.MatKhau = "123";
+            user.ChucVu = "Nhân viên";
+            user.MaNV = 6;
+            userBUS.AddUserBUS(user);
+
+            Assert.AreEqual(4, userBUS.LoadUserBUS().Count);
+        }
+
+        [TestMethod]
+        public void Test_UpdateUser_Suscess()
+        {
+            user = new TaiKhoan();
+            user.MaTaiKhoan = 5;
+            user.TenTaiKhoan = "Thuy123";
+            user.MatKhau = "123";
+            user.ChucVu = "Nhân viên";
+            user.MaNV = 6;
+
+            userBUS = new TaiKhoanBUS();
+            Assert.AreEqual(0, userBUS.SearchTenTaiKhoan(user.TenTaiKhoan).Count);
+
+            userBUS.UpdateUserBUS(user);
+
+            Assert.AreEqual(1, userBUS.SearchTenTaiKhoan(user.TenTaiKhoan).Count);
+        }
     }
 }
