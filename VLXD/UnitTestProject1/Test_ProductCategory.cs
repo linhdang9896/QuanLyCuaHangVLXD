8b4ff61977d5b2b8f5548c73a73f7cb1768e0d00
longlongg <1451010098Long@ou.edu.vn>
2018-11-21 16:36:35 +0700

    test-prcategory-1

--- VLXD/UnitTestProject1/Test_ProductCategory.cs
+++ VLXD/UnitTestProject1/Test_ProductCategory.cs
@@ -12,6 +12,32 @@ namespace UnitTest
     {
         LoaiSanPhamBUS lspBUS;
         LoaiSanPham lsp;
+        [TestMethod]
+        public void Test_AddLoaiSP_Suscess()
+        {
+            lspBUS = new LoaiSanPhamBUS();
+            Assert.AreEqual(6, lspBUS.LoadLoaiSPBUS().Count);
 
+            lsp = new LoaiSanPham();
+            lsp.TenLoai = "lspTest";
+            lspBUS.AddLoaiSPBUS(lsp);
+
+            Assert.AreEqual(7, lspBUS.LoadLoaiSPBUS().Count);
+        }
+
+        [TestMethod]
+        public void Test_UpdateLoaiSP_Suscess()
+        {
+            lsp = new LoaiSanPham();
+            lsp.MaLoaiSP = 5;
+            lsp.TenLoai = "Thép";
+            
+            lspBUS = new LoaiSanPhamBUS();
+            Assert.AreEqual(0, lspBUS.SearchTenLoaiSPBUS(lsp.TenLoai).Count);
+
+            lspBUS.UpdateLoaiSPBUS(lsp);
+
+            Assert.AreEqual(1, lspBUS.SearchTenLoaiSPBUS(lsp.TenLoai).Count);
+        }
     }
 }
