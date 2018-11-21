de560d52a19a96ab4c654ff4d070ddb7dbc1bba9
longlongg <1451010098Long@ou.edu.vn>
2018-11-21 16:40:56 +0700

    test-supplier-1

--- VLXD/UnitTestProject1/Test_Supplier.cs
+++ VLXD/UnitTestProject1/Test_Supplier.cs
@@ -13,5 +13,34 @@ namespace UnitTest
         NhaSanXuatBUS nsxBUS;
         NhaSanXuat nsx;
 
+        [TestMethod]
+        public void Test_AddNSX_Suscess()
+        {
+            nsxBUS = new NhaSanXuatBUS();
+            Assert.AreEqual(4, nsxBUS.LoadNSXBUS().Count);
+
+            nsx = new NhaSanXuat();
+            nsx.TenCongTyNSX = "nsxTest";
+            nsx.DiaChiNSX = "1123 xyz";
+            nsx.DienThoaiNSX = "012345";
+            nsxBUS.AddNSXBUS(nsx);
+
+            Assert.AreEqual(5, nsxBUS.LoadNSXBUS().Count);
+        }
+
+        [TestMethod]
+        public void Test_UpdateNSX_Suscess()
+        {
+            nsx = new NhaSanXuat();
+            nsx.MaNSX = 6;
+            nsx.TenCongTyNSX = "nsxTestUpdate";
+            
+            nsxBUS = new NhaSanXuatBUS();
+            Assert.AreEqual(0, nsxBUS.SearchTenNSXBUS(nsx.TenCongTyNSX).Count);
+
+            nsxBUS.UpdateNSXBUS(nsx);
+
+            Assert.AreEqual(1, nsxBUS.SearchTenNSXBUS(nsx.TenCongTyNSX).Count);
+        }
     }
 }
