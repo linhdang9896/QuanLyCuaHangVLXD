36ed706a082515248dfdbcaae3e65c4a81838f39
longlongg <1451010098Long@ou.edu.vn>
2018-11-21 16:08:27 +0700

    setup hoadonDAO

---
+++ VLXD/DAO/HoaDonDAO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.Entity;
+using DTO;
+
+namespace DAO
+{
+    public class HoaDonDAO
+    {
+        VLXD db = new VLXD();
+    }
+}
