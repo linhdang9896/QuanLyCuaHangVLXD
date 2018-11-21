64945d43a411764ed4a8342566fdbbf8ea18a0e3
longlongg <1451010098Long@ou.edu.vn>
2018-11-21 16:09:43 +0700

    method hd1

--- VLXD/DAO/HoaDonDAO.cs
+++ VLXD/DAO/HoaDonDAO.cs
@@ -12,4 +12,36 @@ namespace DAO
     {
         VLXD db = new VLXD();
     }
+            public List<HD_CTHD> LoadHDDAO()
+        {
+            List<HD_CTHD> result = (from hd in db.HoaDon
+                                    join cthd in db.ChiTietHD
+                                    on hd.MaHD equals cthd.MaHD
+                                    select new HD_CTHD(){
+                                        MaHD =  hd.MaHD,
+                                        MaKH = hd.MaKH,
+                                        MaNV = hd.MaNV,
+                                        NgayDatHang = hd.NgayDatHang,
+                                        NgayGiaoHang = hd.NgayGiaoHang,
+                                        MaSP = cthd.MaSP,
+                                        GiaBan = cthd.GiaBan,
+                                        SoLuong = cthd.SoLuong,
+                                        GiamGia = cthd.GiamGia,
+                                        ThanhTien = cthd.ThanhTien
+                                    }).ToList();
+            return result;
+        }
+
+        public int KtraConHangDAO(int maSPKtra)
+        {
+            SanPham spKtra = db.SanPham.Find(maSPKtra);
+            if (spKtra.SoLuongTon > 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
 }
