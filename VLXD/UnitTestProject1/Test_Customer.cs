40888d17da1645d533a5d48da9fad27a344195eb
longlongg <1451010098Long@ou.edu.vn>
2018-11-21 16:49:43 +0700

    test-customer-1

--- VLXD/UnitTestProject1/Test_Customer.cs
+++ VLXD/UnitTestProject1/Test_Customer.cs
@@ -13,5 +13,40 @@ namespace UnitTest
         KhachHangBUS khBUS;
         KhachHang kh;
 
+        [TestMethod]
+        public void Test_AddKhachHang_Suscess()
+        {
+            khBUS = new KhachHangBUS();
+            Assert.AreEqual(10, khBUS.LoadKHBUS().Count);
+
+            kh = new KhachHang();
+            kh.HoKH = "Nguyễn Thị";
+            kh.TenKH = "th";
+            kh.GioiTinh = "Nu";
+            kh.DiaChi = "123";
+            kh.DienThoai = "321";
+            khBUS.AddKHBUS(kh);
+
+            Assert.AreEqual(11, khBUS.LoadKHBUS().Count);
+        }
+
+        [TestMethod]
+        public void Test_UpdateKhachHang_Suscess()
+        {
+            kh = new KhachHang();
+            kh.MaKH = 1011;
+            kh.HoKH = "Nguyễn Thị";
+            kh.TenKH = "Pass Test";
+            kh.GioiTinh = "Nam";
+            kh.DiaChi = "1234 Nguyễn Văn Linh";
+            kh.DienThoai = "038123246";
+
+            khBUS = new KhachHangBUS();
+            Assert.AreEqual(0, khBUS.SearchTenKHBUS(kh.TenKH).Count);
+
+            khBUS.UpdateKHBUS(kh);
+
+            Assert.AreEqual(1, khBUS.SearchTenKHBUS(kh.TenKH).Count);
+        }
     }
 }
